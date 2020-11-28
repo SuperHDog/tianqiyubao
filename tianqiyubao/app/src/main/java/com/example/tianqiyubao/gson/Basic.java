6547463d8ee1d56a0cc27e610bab9ea5e31052f5
tianqiyubao <1853534858@qq.com>
2020-11-28 08:58:02 +0800

    first commit

---
+++ tianqiyubao/app/src/main/java/com/example/tianqiyubao/gson/Basic.java
@@ -0,0 +1,17 @@
+package com.example.tianqiyubao.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+    public Updata updata;
+    public class Updata{
+        @SerializedName("loc")
+        public String updataTime;
+    }
+}
+
